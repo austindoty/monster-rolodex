98c6f23ccfe6b5d42019d882507880117e0a1e6c
Austin Doty <asrdoty@gmail.com>
2023-11-28 20:18:35 -0500

    Finish Project

--- src/App.js
+++ src/App.js
@@ -1,56 +1,92 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 
 import CardList from './components/card-list/card-list.component';
 import SearchBox from './components/search-box/search-box.component';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [searchField, setSearchField] =useState(''); 
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilterMonsters] =useState(monsters)
 
-    this.state = {
-      monsters:[],
-      searchField:''
-    }
+  useEffect(() => {
+    fetch ('https://jsonplaceholder.typicode.com/users')
+    .then((resposne)=> resposne.json())
+    .then((users) => setMonsters(users))
+  }, []);
+
+  const onSearchChange = (event) => {
+        const searchFieldString = event.target.value.toLocaleLowerCase();
+        setSearchField(searchFieldString);
+        console.log(searchFieldString)
   }
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then ((users)=> this.setState(()=> {
-      return {monsters: users}
-    },
-    ))
-  }
-
-  onSearchChange = (event) => {
-    console.log(event.target.value);
-    const searchField = event.target.value.toLocaleLowerCase();
-    
-    this.setState(()=> {
-      return { searchField };
+  useEffect(()=> {
+    const newFilteredMonsters = monsters.filter((monster)=> {
+      return monster.name.toLocaleLowerCase().includes(searchField);
     });
+  setFilterMonsters(newFilteredMonsters)
+  }, [monsters, searchField])
 
-  }
-  render () {
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
 
-    const filteredMonsters =monsters.filter((monster)=> {
-      return monster.name.toLocaleLowerCase().includes(searchField)
-     });
-
-    return (
-      <div className="App">
+  return (
+    <div className="App">
         <h1 className='app-title'>Monsters Rolodex</h1>
         <SearchBox 
         className='search-box'
         onChangeHandler={onSearchChange} 
-        placeholder='search monsters' /> 
-        <CardList monsters={ filteredMonsters }/>
+        placeholder='search monsters' />
+        <CardList monsters={filteredMonsters} /> 
       </div>
-    );
-  }
+  )
 }
+// class App extends Component {
+//   constructor() {
+//     super();
+
+//     this.state = {
+//       monsters:[],
+//       searchField:''
+//     }
+//   }
+
+//   componentDidMount(){
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(response => response.json())
+//     .then ((users)=> this.setState(()=> {
+//       return {monsters: users}
+//     },
+//     ))
+//   }
+
+//   onSearchChange = (event) => {
+//     console.log(event.target.value);
+//     const searchField = event.target.value.toLocaleLowerCase();
+    
+//     this.setState(()=> {
+//       return { searchField };
+//     });
+
+//   }
+//   render () {
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+
+//     const filteredMonsters =monsters.filter((monster)=> {
+//       return monster.name.toLocaleLowerCase().includes(searchField)
+//      });
+
+//     return (
+//       <div className="App">
+//         <h1 className='app-title'>Monsters Rolodex</h1>
+//         <SearchBox 
+//         className='search-box'
+//         onChangeHandler={onSearchChange} 
+//         placeholder='search monsters' /> 
+//         <CardList monsters={ filteredMonsters }/>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,9 +1,7 @@
-import { Component } from "react";
 import './card.styles.css';
 
-class Card extends Component{
-render() {
-    const { id, name, email } = this.props.monster;
+const Card = ({monster}) => {
+    const { id, name, email } = monster;
     return (
         <div className="card-container" key={id}>
                     <img alt={`monster ${name}`} src ={`https://robohash.org/${id}?set=set2&size=180x180`} />
@@ -12,5 +10,5 @@ render() {
                 </div>
     )
 }
-}
+
 export default Card;
